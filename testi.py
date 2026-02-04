7eaa573f635fe6d67ca5163ecf991419c995f79b
Majlinda <majlinda.raci@metropolia.fi>
2026-02-04 11:43:01 +0200

    1

--- testi.py
+++ testi.py
@@ -1,2 +1,23 @@
 print("hello world")
 print("Hi, Majlinda")
+nimi = input("Anna oma nimesi:")
+print("")
+print("Hello",nimi)
+print("Hello"+nimi)
+print("Hello " + nimi)
+print("")
+print("Hello",nimi,"!")
+print("Hello " + nimi + "!")
+print(f"Hello,{nimi}!")
+nimi2="Python"
+print(f"Hello,{nimi} ja {nimi2}!")
+print(" hello, " + nimi + " ja " + nimi2 + "!")
+ika=22
+print(ika)
+number1=100
+number2= "100"
+ika=int(input("Anna ikäsi:"))
+lisa=10
+print("käyttäjän ikä on:", ika + lisa)
+
+
